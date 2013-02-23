2e6ced8d9df2c1459588df899fc925979df9275c
Paulo Poiati <paulogpoiati@gmail.com>
2013-02-22 22:42:59 -0300

    added install element sentinel

--- js/niziin.js
+++ js/niziin.js
@@ -1,2 +1,11 @@
+function addInstalledSentinel() {
+  var isInstalledElement = document.createElement('div');
+  isInstalledElement.id = 'extension-is-installed';
+  document.body.appendChild(isInstalledElement);
+}
+
 chrome.extension.sendRequest("setExtensionVersion", function(response) {
 });
+
+addInstalledSentinel();
+
