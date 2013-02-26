cf892dc35a347d97290ec4396162ddd83f304184
Paulo Poiati <paulogpoiati@gmail.com>
2013-02-26 00:34:11 -0300

    new version, new ui

--- src/bg/background.js
+++ src/bg/background.js
@@ -12,10 +12,12 @@ var UI = {
     chrome.pageAction.show(currentTabId);
   },
   setPageBookmarkedIcon: function() {
-    chrome.pageAction.setIcon({tabId: currentTabId, path: '../icons/icon19-heart.png'});
+    chrome.pageAction.setIcon(
+      { tabId: currentTabId, path: '../icons/icon19-heart.png' } 
+    );
   },
   setNoPopup: function() {
-    chrome.pageAction.setPopup({tabId: currentTabId, popup: ''});
+    chrome.pageAction.setPopup({ tabId: currentTabId, popup: '' });
   }
 };
 
@@ -47,33 +49,14 @@ function _inspectPage(tabId, changeInfo, tab) {
 };
 
 function _sendRequest(method, params, endpoint, onSuccess, onError) {
-  var request = new XMLHttpRequest();
-
-  if (method == 'POST') {
-    request.open(method, 'http://nizi.in/a' + endpoint, true);
-  } else {
-    request.open(method, 'http://nizi.in/a' + endpoint + '?' + params, true);
-  }
-
-  request.setRequestHeader('Content-Type' , 'application/x-www-form-urlencoded');
-  request.setRequestHeader('Accept' , 'application/json');
-  if (method == 'POST') {
-    request.send(params);
-  } else {
-    request.send();
-  }
-  request.onreadystatechange = function() {
-    if (request.readyState == 4 && request.status == 200) {
-      response = JSON.parse(request.responseText);
-      if (response.success) {
-        onSuccess(response);
-      } else {
-        onError(response.message);
-      }
-    } else {
-      console.log(request);
-    }
-  };
+  $.ajax({
+    type: method,
+    url: 'http://nizi.in/a' + endpoint,
+    dataType: 'json',
+    data: params,
+    success: onSuccess,
+    error: onError
+  });
 }
 
 function updatePage(fn) {
@@ -92,17 +75,21 @@ function checkBookmarkExistense(href) {
   var onError = function() {
   };
 
-  _sendRequest('GET', 'href=' + escape(href), '/bookmark/exists/', onSuccess, onError);
+  _sendRequest('GET', { href: href }, '/bookmark/exists/', onSuccess, onError);
 }
 
 function addBookmark(expression, onSuccess, onError) {
-  var onSuccessWrapper = function() {
-    onSuccess();
-    UI.setPageBookmarkedIcon();
-    UI.setNoPopup();
+  var onSuccessWrapper = function(response) {
+    if (response.success) {
+      onSuccess();
+      UI.setPageBookmarkedIcon();
+      UI.setNoPopup();
+      return;
+    } 
+    onError(response.message);
   };
 
-  _sendRequest('POST', 'expression=' + expression, '/bookmark/', onSuccessWrapper, onError);
+  _sendRequest('POST', { expression: expression }, '/bookmark/', onSuccessWrapper, onError);
 }
 
 chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -2,6 +2,7 @@ $(function() {
   var CLOSE_POPUP_DELAY = 1500;
 
   var backgroundPage = chrome.extension.getBackgroundPage(),
+      address = $('#address'),
       input = $('#expression'),
       help = $('.help');
 
@@ -10,17 +11,14 @@ $(function() {
   };
 
   var _expression = function(event) {
-    return input.val();
+    return address.text() + ' ' + input.val();
   };
 
   var _onSuccess = function() {
-    input.hide();
+    $('.controls').hide();
     help
       .addClass('help-success')
-      .find('p')
-        .text('Bookmark added successfully!').parent()
-      .find('a')
-        .hide();
+      .text('Bookmark added successfully!');
 
     setTimeout(window.close, CLOSE_POPUP_DELAY);
   };
@@ -28,19 +26,23 @@ $(function() {
   var _onError = function(message) {
     help
       .addClass('help-error')
-      .find('p')
-        .text(message);
+      .text(message);
+  };
+
+  var _addBookmark = function() {
+    backgroundPage.addBookmark(_expression(), _onSuccess, _onError);
   };
 
   backgroundPage.updatePage(function(uri) {
-    setTimeout(function() {
-      input.val(uri + ' ').focus();
-    }, 50);
+    address.text(uri);
+    input.focus();
   });
 
   input.on('keypress', function(event) {
     if (_returnPressed(event)) {
-      backgroundPage.addBookmark(_expression(), _onSuccess, _onError);
+      _addBookmark();
     }
   });
+
+  $('button').on('click', _addBookmark);
 });
